2a57882385d3000bb438e062e28bf3f301b6c8f9
Nikko <56482580+gnikkoch96@users.noreply.github.com>
2021-07-17 22:23:23 -0700

    small clean up

--- Project_Two/main.py
+++ Project_Two/main.py
@@ -13,61 +13,66 @@ import graph
 import gameboard
 import csv
 
-# Creates the Gameboard (Using a 2D Array)
+# Functions
+def create_graph(gameboard):
+    # Creates the Graph of the Gameboard
+    g = graph.Graph()
+
+    # create a vertex for all chars on the maze
+    for row in range(len(gb.maze_to_array)):
+        for column in range(len(gb.maze_to_array[row])):
+            g.add_vertex(g.num_vertices, gb.maze_to_array[row][column])
+
+    # create vertex and add edges to all "_", ".", and "P"
+    for node_id in g.get_vertices():
+        current_node = g.vert_dict[node_id].get_node()
+        if current_node != '%':
+
+            # check up
+            validate_value = node_id - gb.get_col_count()
+            if validate_value >= 0:
+                up_node_id = validate_value
+                up_node = g.vert_dict[up_node_id].get_node()
+                if up_node != '%':
+                    g.add_edge(node_id, up_node_id)
+
+            # check down
+            validate_value = node_id + gb.get_col_count()
+            if validate_value < gb.get_row_count() * gb.get_col_count():
+                down_node_id = validate_value
+                down_node = g.vert_dict[down_node_id].get_node()
+                if down_node != '%':
+                    g.add_edge(node_id, down_node_id)
+
+            # check left
+            validate_value = node_id - 1
+            if validate_value >= 0:
+                left_node_id = validate_value
+                left_node = g.vert_dict[left_node_id].get_node()
+                if left_node != '%':
+                    g.add_edge(node_id, left_node_id)
+
+            # check right
+            validate_value = node_id + 1
+            if validate_value < gb.get_row_count() * gb.get_col_count():
+                right_node_id = validate_value
+                right_node = g.vert_dict[right_node_id].get_node()
+                if right_node != '%':
+                    g.add_edge(node_id, right_node_id)
+    return g
+
+
+# Main
+
+# Gameboard class is used to convert the maze file into a 2d array
 gb = gameboard.GameBoard()
+
 # row_p, col_p = gb.getStart()
 # print(row_p, col_p)
 
 # row_g, col_g = gb.getGoal()
 # print(row_g, col_g)
 
-# Creates the Graph of the Gameboard
-g = graph.Graph()
-
-# create a vertex for all chars on the maze
-for row in range(len(gb.maze_to_array)):
-    for column in range(len(gb.maze_to_array[row])):
-        g.add_vertex(g.num_vertices, gb.maze_to_array[row][column])
-
-# create vertex and add edges to all "_", ".", and "P"
-for node_id in g.get_vertices():
-    # Nikko: we can use modulus to see what column it is at so if we wanted to look up from 219 we would do 219%22 = 21
-    # print(g.vert_dict[node_id].get_node())
-    # validate_value = node_id - (node_id % gb.get_col_count())
-
-    current_node = g.vert_dict[node_id].get_node()
-    if current_node != '%':
-
-        # check up
-        validate_value = node_id - gb.get_col_count()
-        if validate_value >= 0:
-            up_node_id = validate_value
-            up_node = g.vert_dict[up_node_id].get_node()
-            if up_node != '%':
-                g.add_edge(node_id, up_node_id)
-
-        # check down
-        validate_value = node_id + gb.get_col_count()
-        if validate_value < gb.get_row_count() * gb.get_col_count():
-            down_node_id = validate_value
-            down_node = g.vert_dict[down_node_id].get_node()
-            if down_node != '%':
-                g.add_edge(node_id, down_node_id)
-
-        # check left
-        validate_value = node_id - 1
-        if validate_value >= 0:
-            left_node_id = validate_value
-            left_node = g.vert_dict[left_node_id].get_node()
-            if left_node != '%':
-                g.add_edge(node_id, left_node_id)
-
-        # check right
-        validate_value = node_id + 1
-        if validate_value < gb.get_row_count() * gb.get_col_count():
-            right_node_id = validate_value
-            right_node = g.vert_dict[right_node_id].get_node()
-            if right_node != '%':
-                g.add_edge(node_id, right_node_id)
-
+# Creates the graph
+g = create_graph(gb)
 g.graph_summery()
