bd344240184d0caa6fb1bbe3bf1e6f33e9ab1a74
rbaranwal01 <rishikabaranwal01@gmail.com>
2021-07-29 19:22:14 -0700

    Small change for console menu
    Menu repeats until user enters the exit option

--- Project_Three/main.py
+++ Project_Three/main.py
@@ -20,24 +20,26 @@ if __name__ == '__main__':
 
     root_node = dt.tree.root
 
-    user_input = int(input("Which algorithm do you want to perform?\n1.Minimax\n2.Alpha-Beta Pruning\n3.Exit\nChoice:"))
+    user_input = 0
+    while user_input < 3:
+        user_input = int(input("\nWhich algorithm do you want to perform?\n1.Minimax\n2.Alpha-Beta Pruning\n3.Exit\nChoice: "))
 
-    if user_input == 1: # perform minimax (w/out pruning)
-        # for depth, you have to subtract by one because of computer indexing
-        dt.tree.minimax(root_node, 2, False)
+        if user_input == 1: # perform minimax (w/out pruning)
+            # for depth, you have to subtract by one because of computer indexing
+            dt.tree.minimax(root_node, 2, False)
 
-        # print out all the nodes along with their values
-        for node_key in dt.tree.get_nodes():
-            node = dt.tree.node_dict[node_key]
-            print(node.get_id(), "[", node.get_value(), "]")
+            # print out all the nodes along with their values
+            for node_key in dt.tree.get_nodes():
+                node = dt.tree.node_dict[node_key]
+                print(node.get_id(), "[", node.get_value(), "]")
 
-    elif user_input == 2: # perform alpha-beta pruning
-        # for depth, you have to subtract by one because of computer indexing
-        dt.tree.minimax(root_node, 2, True)
+        elif user_input == 2: # perform alpha-beta pruning
+            # for depth, you have to subtract by one because of computer indexing
+            dt.tree.minimax(root_node, 2, True)
 
-        # print out all the nodes along with their values and their pruned status
-        for node_key in dt.tree.get_nodes():
-            node = dt.tree.node_dict[node_key]
-            print(node.get_id(), "[", node.get_value(), "], Pruned Status: ", node.pruned)
-    else: # exit the program
-        print("Exited Program...")
+            # print out all the nodes along with their values and their pruned status
+            for node_key in dt.tree.get_nodes():
+                node = dt.tree.node_dict[node_key]
+                print(node.get_id(), "[", node.get_value(), "], Pruned Status: ", node.pruned)
+        else: # exit the program
+            print("Exited Program...")
